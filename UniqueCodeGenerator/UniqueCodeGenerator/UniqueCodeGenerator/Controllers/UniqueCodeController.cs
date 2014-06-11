6d9e91a8fe22b0feb6847e2e54ffdc68b7fe5a9b
sreeharsha <harsha@indavest.com>
2014-06-11 11:37:27 +0530

    Unique Code Generator

---
+++ UniqueCodeGenerator/UniqueCodeGenerator/UniqueCodeGenerator/Controllers/UniqueCodeController.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace UniqueCodeGenerator.Controllers
+{
+    public class UniqueCodeController : Controller
+    {
+
+        private Repository repo = new Repository();
+        private loyaltyUniqueCodes _context = new loyaltyUniqueCodes();
+        //
+        // GET: /UniqueCode/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult GenerateUniqueCodes(int numberOfKeys)
+        {
+            string[] keys = new string[numberOfKeys];
+
+            for (int loop = 0; loop < numberOfKeys; loop++)
+            {
+                keys[loop] = this.RNGCharacterMask();
+            }
+
+            repo.UniqueCode_BulkInsert(keys, 67);
+            return View("Generate");
+        }
+
+        private string RNGCharacterMask()
+        {
+            int maxSize = 8;
+
+            char[] chars = new char[62];
+            string a;
+
+            a = "abcdefghjkmnpqrstuvwxyz23456789";
+            chars = a.ToCharArray();
+
+            int size = maxSize;
+            byte[] data = new byte[1];
+
+            RNGCryptoServiceProvider crypto = new RNGCryptoServiceProvider();
+            crypto.GetNonZeroBytes(data);
+
+            size = maxSize;
+            data = new byte[size];
+            crypto.GetNonZeroBytes(data);
+
+            StringBuilder result = new StringBuilder(size);
+
+            foreach (byte b in data)
+            {
+                result.Append(chars[b % (chars.Length - 1)]);
+            }
+
+            return result.ToString();
+        }
+
+        private string RNGCharacterMaskCouponCode()
+        {
+            int maxSize = 8;
+
+            char[] chars = new char[62];
+            string a;
+
+            a = "1234567890";
+            chars = a.ToCharArray();
+
+            int size = maxSize;
+            byte[] data = new byte[1];
+
+            RNGCryptoServiceProvider crypto = new RNGCryptoServiceProvider();
+            crypto.GetNonZeroBytes(data);
+
+            size = maxSize;
+            data = new byte[size];
+            crypto.GetNonZeroBytes(data);
+
+            StringBuilder result = new StringBuilder(size);
+
+            foreach (byte b in data)
+            {
+                result.Append(chars[b % (chars.Length - 1)]);
+            }
+
+            return result.ToString();
+        }
+
+        public string GenerateCouponCode()
+        {
+            return this.RNGCharacterMaskCouponCode();
+        }
+
+
+        public void AddCouponsToDatabase()
+        {
+            try
+            {
+                List<Guid> guids = new List<Guid>();
+                for (int i = 0; i < 20000; i++)
+                {
+                    string couponCode = this.GenerateCouponCode();
+                    Generated_CouponCode ccExist = repo.GetCouponCode(couponCode);
+                    if (ccExist != null)
+                    {
+                        Response.Write(ccExist.CouponCode + "<br>");
+                        continue;
+                    }
+                    Generated_CouponCode cc = new Generated_CouponCode();
+                    cc.CouponCode = couponCode;
+                    cc.RestaurantId = 67;
+                    cc.EmailAddress = "test@test.com";
+                    cc.IsAssigned = false;
+                    cc.IsRedeemed = false;
+                    cc.DateCreated = DateTime.Now;
+                    cc.RewardId = 1;
+
+                    _context.AddToGenerated_CouponCode(cc);
+                    _context.SaveChanges();
+
+                }
+            }
+            catch (Exception ex)
+            {
+                Response.Write(ex.Message);
+            }
+            
+        }
+    }
+}
---
+++ UniqueCodeGenerator/UniqueCodeGenerator/UniqueCodeGenerator/Repository.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Transactions;
+using System.Diagnostics;
+using System.Data.SqlClient;
+using System.Data.Objects;
+
+namespace UniqueCodeGenerator
+{
+    public class Repository
+    {
+        private loyaltyUniqueCodes _context = new loyaltyUniqueCodes();
+        /// <summary>
+        /// Insert Unique Codes
+        /// </summary>
+        /// <param name="uniqueCodes"></param>
+        /// <param name="restaurant"></param>
+        /// <returns></returns>
+        public bool UniqueCode_BulkInsert(string[] uniqueCodes, int restaurantId)
+        {
+            List<Generated_UniqueCode> UniqueCodeList = new List<Generated_UniqueCode>();
+
+            foreach (string code in uniqueCodes)
+            {
+                Generated_UniqueCode generated_UniqueCode = new Generated_UniqueCode();
+                generated_UniqueCode.UniqueCode = code;
+                generated_UniqueCode.DateCreated = DateTime.Today;
+                generated_UniqueCode.RestaurantId = restaurantId;
+                generated_UniqueCode.IsValidated = false;
+                generated_UniqueCode.DateValidated = null;
+
+                UniqueCodeList.Add(generated_UniqueCode);
+            }
+
+            try
+            {
+                int count = 0;
+                foreach (var code in UniqueCodeList)
+                {
+                    _context.Generated_UniqueCode.AddObject(code);
+                    count++;
+
+                    //if ((count % 100) == 0)
+                    //{
+                        //commit changes to database
+                        //this.Save();
+                    //}
+                }
+
+                //commit changes to database
+                //this.Save();
+                _context.SaveChanges();
+                
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Trace.WriteLine(ex.Message);
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Commit changes to databse
+        /// </summary>
+        private void Save()
+        {
+            _context.SaveChanges();
+        }
+
+
+        public Generated_CouponCode GetCouponCode(string couponCode)
+        {
+            try
+            {
+                var generated_CouponCode = _context.Generated_CouponCode.FirstOrDefault(c => c.CouponCode == couponCode);
+                return generated_CouponCode;
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+    }
+}
